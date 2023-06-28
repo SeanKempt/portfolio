d08de98824bf743e8de083c06e8026e94c70a29c
SeanKempt <kempt101@gmail.com>
2023-06-28 16:52:55 -0500

    feat: get initial files established

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,53 @@
 module.exports = {
-  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/jsx-runtime',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'off',
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': [1, { ignoreRestSiblings: true }],
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': [
+      'warn',
+      {
+        ignoreDeclarationMerge: true,
+      },
+    ],
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    // this is covered by the typescript compiler, so we don't need it
+    'no-undef': 'off',
+    'no-shadow': 'off', // TS does it
   },
-}
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  overrides: [
+    {
+      files: ['vite.config.ts', '.eslintrc.cjs', 'tailwind.config.js', 'postcss.config.js'],
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        project: './tsconfig.node.json',
+      },
+    },
+  ],
+};
